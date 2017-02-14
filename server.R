91f2547e0d001e1be0e816ae78c0e625184d17ff
Giang Nguyen Rudderham <giang-rudderham@users.noreply.github.com>
2017-02-14 01:50:26 -0600

    Update server.R

--- server.R
+++ server.R
@@ -2,92 +2,52 @@ library(shiny)
 library(rgdal) # for readOGR
 library(leaflet)
 
-#read shape file
+#read shapefile
 dprk.shp <- readOGR(dsn = "PRK_adm", layer = "PRK_adm1") #SpatialPolygonsDF
 
 #read data
 data <- read.csv("popdensity.csv")
 
-
-dprk.shp$Province <- dprk.shp$NAME_1
+#Merge shapefile with DPRK data
+dprk.shp$Province <- dprk.shp$NAME_1 #create "Province" to merge by "Province";
 dprk.shp@data <- merge(dprk.shp@data, data, by = "Province")
 
+# Create color pallete
+binpal <- colorBin(palette = "Reds", domain = dprk.shp$PopDensity,
+                   bins = c(0, 200, 400, 600, 800, 1000, 4500), pretty = FALSE)
 
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output) {
   
-  # reactive input
-  input_year <- reactive({
-    paste0("X", input$yearmap, collapse = "") 
-  })
-  
-  # static part of the map
-  output$worldmap <- renderLeaflet({
-    leaflet() %>% #create a leaflet object
-      addTiles() %>% #add a base map
-      setView(lng = 0, lat = 55, zoom = 2)
-  })
+  #static part of the map
+  output$dprkmap <- renderLeaflet({
+    leaflet () %>%
+      setView(lng = 128, lat = 40, zoom = 7) %>%
+      addPolygons(data = dprk.shp, stroke = TRUE, color = "black", weight = 1,
+                  smoothFactor = 1,
+                  fillOpacity = 0.5, fillColor = binpal(dprk.shp$PopDensity),
+                  popup = paste("Province:", dprk.shp$Display, "<br>",
+                                "Population Density:", dprk.shp$PopDensity),
+                  group = "density") %>%
+      addLegend(position = "bottomright", pal = binpal, 
+                values = dprk.shp$PopDensity, opacity = 0.5,
+                title = "Population Density") %>%
+      addMarkers(lng = 125.8, lat = 38.97, 
+                 popup = "The Capital: Pyongyang <br> Population Density: 4216.69")
+  })  
   
   # put dynamic parts of the map in observers
-  ## add polygons for pop. undernourished
+  ## add circles
   observe({
-    chosen_year <- input_year()
-    binpal <- colorBin(palette = "Reds", domain = joined.to.map[[chosen_year]],
-                       bins = c(0, 20, 40, 60, 80, 100), pretty = FALSE)
-    leafletProxy("worldmap", data = joined.to.map) %>%
-      clearShapes() %>% 
-      addPolygons(stroke = FALSE, smoothFactor = 1, fillOpacity = 0.5,
-                  color = binpal(joined.to.map[[chosen_year]]),
-                  popup = paste("Country:", joined.to.map$Country, "<br>",
-                                "Undernourished:", joined.to.map[[chosen_year]],
-                                "%"),
-                  group = "undernourished"
-      )          
-  })
-  
-  ## add legend for polygons
-  observe({
-    chosen_year <- input_year()
-    binpal <- colorBin(palette = "Reds", domain = joined.to.map[[chosen_year]],
-                       bins = c(0, 20, 40, 60, 80, 100), pretty = FALSE)
-    
-    leafletProxy("worldmap", data = joined.to.map) %>%
-      clearControls() %>%
-      addLegend(position = "bottomright", pal = binpal,
-                values = joined.to.map[[chosen_year]], opacity = 0.5,
-                title = "Population undernourished (%)"
-      )
-  })
-  
-  ## add markers for pop. access sanitation
-  observe({
-    proxy <- leafletProxy("worldmap", data = sanitation)
-    proxy %>% clearMarkers()
-    if (input$sanitation) {
-      chosen_year <- input_year()
+    proxy <- leafletProxy("dprkmap", data = dprk.shp)
+    proxy %>% clearGroup(group = "electricity")
+    if (input$cooking) {
       proxy %>%
-        addCircleMarkers(lng = ~long, lat = ~lat, stroke = TRUE, 
-                         color = "#03F", weight = 1, opacity = 3,
-                         fillOpacity = 0.2, 
-                         radius = ( 101 - sanitation[[chosen_year]] ) / 5,
-                         popup = paste("Country:", sanitation$Country, "<br>",
-                                       "Sanitation facility:", 
-                                       sanitation[[chosen_year]], "%"),
-                         group = "sanitation")
+        addCircles(data = dprk.shp, lng = ~long, lat = ~lat, weight = 1, opacity = 3,
+                   radius = sqrt(dprk.shp$PercElectricity / pi) * 30000, 
+                   popup = paste("Province:", dprk.shp$Display, "<br>",
+                                 "Households that use electricity as cooking fuel:",
+                                 dprk.shp$PercElectricity, "%"),
+                   group = "electricity")
     }
   })
-  
-  
-  
-  
-  output$hist <- renderPlot({
-    chosen_hist <- paste0("X", input$yearhist, collapse = "")
-    hist(data[[chosen_hist]],
-         breaks = c(0, 20, 40, 60, 80, 100),
-         col = "grey",
-         ylim = c(0, 140),
-         xlab = "Population Undernourished (%)",
-         main = "Histogram of Population Undernourished")
-    box()
-  })
 })
-
