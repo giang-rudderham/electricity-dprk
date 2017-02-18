5b4ff994e10c4a12dbe14106b61b3971c059a61e
Giang Nguyen Rudderham <giang-rudderham@users.noreply.github.com>
2017-02-18 04:52:52 -0600

    Add layerId

--- server.R
+++ server.R
@@ -2,8 +2,8 @@ library(shiny)
 library(rgdal) # for readOGR
 library(leaflet)
 
-# Version 2: Update map with clicking #########################################
-########################################
+# Version 3: update map with clicking on the provinces #################
+################################################
 
 #read shapefile
 dprk.shp <- readOGR(dsn = "PRK_adm", layer = "PRK_adm1") #SpatialPolygonsDF
@@ -30,7 +30,8 @@ shinyServer(function(input, output) {
                   fillOpacity = 0.5, fillColor = binpal(dprk.shp$PopDensity),
                   popup = paste("Province:", dprk.shp$Display, "<br>",
                                 "Population Density:", dprk.shp$PopDensity),
-                  group = "density") %>%
+                  group = "density",
+                  layerId = ~Display) %>%
       addLegend(position = "bottomright", pal = binpal, 
                 values = dprk.shp$PopDensity, opacity = 0.5,
                 title = "Population Density") %>%
@@ -56,7 +57,8 @@ shinyServer(function(input, output) {
                    popup = paste("Province:", dprk.shp$Display, "<br>",
                                  "Households that use electricity as cooking fuel:",
                                  dprk.shp$PercElectricityCooking, "%"),
-                   group = "electricitycooking")
+                   group = "electricitycooking",
+                   layerId = ~Display)
     }
   })
   
@@ -72,86 +74,22 @@ shinyServer(function(input, output) {
                    popup = paste("Province:", dprk.shp$Display, "<br>",
                                  "Households that use electronic heating:",
                                  dprk.shp$PercElectronicHeating, "%"),
-                   group = "electronicheating")
+                   group = "electronicheating", 
+                   layerId = ~Display)
     }
   })
+  
+  ## clicking on marker at Pyongyang generates a marker at South Hamgyong
+  observeEvent(input$dprkmap_shape_click, {
+    proxy <- leafletProxy("dprkmap", data = dprk.shp)
+    p <- input$dprkmap_shape_click
+    proxy %>% 
+      clearGroup(group = "testpopup") %>%
+      addMarkers(lng = 128.30, lat = 40.37, popup = paste(p$lng, p$lat, p$id), group = "testpopup")
+    
+  })
+  
+  #output$histCooking <- renderPlot({
+   # plot(rnorm(23), rnorm(23))
+  #})
 })
-
-
-
-
-
-# Version 1: Check boxes for electricity cooking and electronic heating #######
-###################################################################
-
-# #read shapefile
-# dprk.shp <- readOGR(dsn = "PRK_adm", layer = "PRK_adm1") #SpatialPolygonsDF
-# 
-# #read data
-# data <- read.csv("popdensity.csv")
-# 
-# #Merge shapefile with DPRK data
-# dprk.shp$Province <- dprk.shp$NAME_1 #create "Province" to merge by "Province";
-# dprk.shp@data <- merge(dprk.shp@data, data, by = "Province")
-# 
-# # Create color pallete
-# binpal <- colorBin(palette = "Reds", domain = dprk.shp$PopDensity,
-#                    bins = c(0, 200, 400, 600, 800, 1000, 4500), pretty = FALSE)
-# 
-# shinyServer(function(input, output) {
-#   
-#   #static part of the map
-#   output$dprkmap <- renderLeaflet({
-#     leaflet () %>%
-#       setView(lng = 128, lat = 40, zoom = 7) %>%
-#       addPolygons(data = dprk.shp, stroke = TRUE, color = "black", weight = 1,
-#                   smoothFactor = 1,
-#                   fillOpacity = 0.5, fillColor = binpal(dprk.shp$PopDensity),
-#                   popup = paste("Province:", dprk.shp$Display, "<br>",
-#                                 "Population Density:", dprk.shp$PopDensity),
-#                   group = "density") %>%
-#       addLegend(position = "bottomright", pal = binpal, 
-#                 values = dprk.shp$PopDensity, opacity = 0.5,
-#                 title = "Population Density") %>%
-#       addMarkers(lng = 125.8, lat = 38.97, 
-#                  popup = paste("The Capital: Pyongyang <br> Population Density:",
-#                           dprk.shp$PopDensity[dprk.shp$Display == "Pyongyang"], "<br>",
-#                           "Households that use electricity as cooking fuel:",
-#                           dprk.shp$PercElectricityCooking[dprk.shp$Display == "Pyongyang"], "%",
-#                           "<br>", "Households that use electronic heating:",
-#                           dprk.shp$PercElectronicHeating[dprk.shp$Display == "Pyongyang"], "%")
-#                  )
-#   })  
-#   
-#   # put dynamic parts of the map in observers
-#   ## add circles for cooking fuel
-#   observe({
-#     proxy <- leafletProxy("dprkmap", data = dprk.shp)
-#     proxy %>% clearGroup(group = "electricitycooking")
-#     if (input$cooking) {
-#       proxy %>%
-#         addCircles(data = dprk.shp, lng = ~long, lat = ~lat, weight = 1, opacity = 3,
-#                    radius = sqrt(dprk.shp$PercElectricityCooking / pi) * 30000, 
-#                    popup = paste("Province:", dprk.shp$Display, "<br>",
-#                                  "Households that use electricity as cooking fuel:",
-#                                  dprk.shp$PercElectricityCooking, "%"),
-#                    group = "electricitycooking")
-#     }
-#   })
-#   
-#   ## add cricles for heating system
-#   observe({
-#     proxy <- leafletProxy("dprkmap", data = dprk.shp)
-#     proxy %>% clearGroup(group = "electronicheating")
-#     if (input$heating) {
-#       proxy %>%
-#         addCircles(data = dprk.shp, lng = ~long, lat = ~lat, weight = 1, opacity = 3,
-#                    radius = sqrt(dprk.shp$PercElectronicHeating / pi) * 30000,
-#                    fillColor = "green", color = "green",
-#                    popup = paste("Province:", dprk.shp$Display, "<br>",
-#                                  "Households that use electronic heating:",
-#                                  dprk.shp$PercElectronicHeating, "%"),
-#                    group = "electronicheating")
-#     }
-#   })
-# })
